a41aceabcfb816c95b041bd0e5c872328db361d6
Hugo Jeller <hugo_jeller@hotmail.com>
2020-10-27 00:17:45 -0400

    Removed unecessary z-index classes

--- resources/js/components/Modal.tsx
+++ resources/js/components/Modal.tsx
@@ -8,15 +8,15 @@ export interface ModalProps {
 
 export const Modal: React.FC<ModalProps> = ({children, open, onClose}) => {
     return <Portal>
-        <div
-            onClick={onClose}
-            className={`transition-all duration-300 ${open ? 'opacity-50 z-20' : 'opacity-0 z-0'} fixed top-0 bottom-0 left-0 right-0 bg-black bg-opacity-50 shadow-menu`}
-        />
-
-        <div className={`pb-8 z-50 transition-transform duration-500
-                transform ${!open && 'translate-y-full'} fixed left-0 bottom-0 right-0 py-2 bg-gray-100`}
+        <div className={`pb-8 transition-transform duration-500
+                transform ${!open && 'translate-y-full'} fixed left-0 bottom-0 right-0 py-2 bg-gray-100 z-50`}
         >
             {children}
         </div>
+
+        <div
+            onClick={onClose}
+            className={`transition-all duration-300 ${open ? 'opacity-50 z-40' : 'opacity-0 z-0'} fixed top-0 bottom-0 left-0 right-0 bg-black bg-opacity-75 shadow-menu`}
+        />
     </Portal>
 };
