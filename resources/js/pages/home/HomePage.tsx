fe2f70dfde22ba905717d856e83eebdad05cda60
Hugo Jeller <hugo_jeller@hotmail.com>
2020-10-27 01:10:43 -0400

    Removed tags and favorites from the homepage

--- resources/js/pages/home/HomePage.tsx
+++ resources/js/pages/home/HomePage.tsx
@@ -18,32 +18,6 @@ export const HomePage: React.FC = () => {
 
     return <>
         <div className="mx-auto container">
-            <div className="mb-4 overflow-x-auto">
-                <div className="flex space-x-4">
-                    {['Frutas', 'Verduras', 'Legumes', 'a', 'b', 'd'].map(name => (
-                        <button className="transition-colors duration-150
-                             flex items-stretch py-2 px-5
-                             text-primary-500 font-medium
-                             bg-primary-100 border-2 border-primary-500 rounded-full
-                             rounded-lg cursor-pointer"
-                        >
-                            {name}
-                        </button>
-                    ))}
-                </div>
-            </div>
-
-            <div className="mb-16">
-                {/* Header */}
-                <div className="flex justify-between items-baseline mb-8">
-                    <Title>Favoritos</Title>
-                    <Link to="/favoritos" className="text-gray-500">Ver todos ›</Link>
-                </div>
-
-                {/* Items */}
-                <ProductList products={Object.values(products.products).filter(product => favorites.favorites.indexOf(product.id) >= 0)}/>
-            </div>
-
             <div>
                 {/* Header */}
                 <div className="flex justify-between items-baseline mb-8">
