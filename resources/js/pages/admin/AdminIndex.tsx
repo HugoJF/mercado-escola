f59fb0923329b77f534681acf354ab753ac58a10
Hugo Jeller <hugo_jeller@hotmail.com>
2020-11-24 14:45:27 -0400

    Reworked admin index

--- resources/js/pages/admin/AdminIndex.tsx
+++ resources/js/pages/admin/AdminIndex.tsx
@@ -1,8 +1,11 @@
-import React                                       from "react";
-import {Title}                                     from "../../components/ui/Title";
-import {Calendar, Icon, ShoppingBag, Truck, Users} from "react-feather";
-import {Link}                                      from "react-router-dom";
-import useRelativePath                             from "../../hooks/useRelativePath";
+import React                                                     from "react";
+import {Title}                                                   from "../../components/ui/Title";
+import {Calendar, ChevronRight, Icon, ShoppingBag, Truck, Users} from "react-feather";
+import {Link}                                                    from "react-router-dom";
+import useRelativePath                                           from "../../hooks/useRelativePath";
+import {DivOrLink}                                               from "../../components/DivOrLink";
+import classNames                                                from "classnames";
+import {Skeleton}                                                from "../../components/ui/Skeleton";
 
 type ButtonType = { title: string, icon: Icon, to: string }
 
@@ -10,10 +13,6 @@ const buttons: ButtonType[] = [{
     title: 'Produtos',
     icon: ShoppingBag,
     to: '/produtos',
-}, {
-    title: 'Pedidos',
-    icon: Truck,
-    to: '/pedidos',
 }, {
     title: 'Produtores',
     icon: Users,
@@ -31,16 +30,25 @@ export const AdminIndex: React.FC = () => {
         <div className="mx-auto container">
             <Title>Administrativo</Title>
 
-            <div className="grid grid-cols-2 gap-8 mt-8">
-                {buttons.map((button, i) => (
-                    <Link
-                        key={i}
-                        to={relative(button.to)}
-                        className="flex flex-col items-center px-4 py-6 bg-gray-200 border rounded-lg shadow-sm"
+            <div className="mt-4">
+                {buttons.map(({icon: Icon, title, to}) => (
+                    <DivOrLink
+                        key={to}
+                        isLink={true}
+                        to={relative(to)}
+                        className={classNames(
+                            `transition-colors duration-150 w-full px-4 py-4 flex items-center
+                            border-b last:border-b-0 border-gray-200 hover:bg-gray-200 cursor-pointer`
+                        )}
                     >
-                        <button.icon size={40} className="mb-3 text-gray-400"/>
-                        <h2 className="text-gray-700 font-medium">{button.title}</h2>
-                    </Link>
+                        <div className="flex items-center justify-center w-6 mr-4">
+                            <Icon className="text-primary-500"/>
+                        </div>
+                        <div className="flex-grow">
+                            <h3 className="text-lg font-medium">{title}</h3>
+                        </div>
+                        <ChevronRight className="flex-shrink-0 text-gray-500"/>
+                    </DivOrLink>
                 ))}
             </div>
         </div>
