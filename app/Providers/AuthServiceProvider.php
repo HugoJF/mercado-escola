0ae79eec2e433aafc68329108a7cd233d9b93793
Hugo Jeller <hugo_jeller@hotmail.com>
2021-01-02 19:44:47 -0400

    Added missing Favorite and Order policies registration

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,8 +3,12 @@
 namespace App\Providers;
 
 use App\Models\Address;
+use App\Models\Favorite;
+use App\Models\Order;
 use App\Models\Product;
 use App\Policies\AddressPolicy;
+use App\Policies\FavoritePolicy;
+use App\Policies\OrderPolicy;
 use App\Policies\ProductPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -19,6 +23,8 @@ class AuthServiceProvider extends ServiceProvider
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
         Product::class => ProductPolicy::class,
         Address::class => AddressPolicy::class,
+        Order::class => OrderPolicy::class,
+        Favorite::class => FavoritePolicy::class,
     ];
 
     /**
