c7bb73262fbb694486a5d704a4c3b38a1c7bc97b
Hugo Jeller <hugo_jeller@hotmail.com>
2021-01-05 17:14:12 -0400

    Fixes horizontal overflow on transitions

--- resources/js/containers/Container.tsx
+++ resources/js/containers/Container.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const Container: React.FC = ({children}) => {
-    return <div className="absolute w-full h-full mx-auto overflow-y-auto">
+    return <div className="absolute w-full h-full mx-auto overflow-x-hidden overflow-y-auto">
         {children}
     </div>
 };
