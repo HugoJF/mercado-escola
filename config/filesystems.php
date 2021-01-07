679b0d378e861f47be8aa0c39932f39d0690dd45
Hugo Jeller <hugo_jeller@hotmail.com>
2021-01-07 02:51:31 -0400

    Added Minio as a filesystem for media

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,28 +45,38 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root'   => storage_path('app'),
+        ],
+
+        'media' => [
+            'driver'                  => 's3',
+            'region'                  => 'hetzner',
+            'endpoint'                => env('MINIO_ENDPOINT'),
+            'key'                     => env('MINIO_ACCESS_KEY_ID'),
+            'secret'                  => env('MINIO_SECRET_ACCESS_KEY'),
+            'bucket'                  => env('MINIO_BUCKET'),
+            'use_path_style_endpoint' => true,
         ],
 
         'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'driver'     => 'local',
+            'root'       => storage_path('app/public'),
+            'url'        => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
         'tests' => [
             'driver' => 'local',
-            'root' => storage_path('tests'),
+            'root'   => storage_path('tests'),
         ],
 
         's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
+            'driver'   => 's3',
+            'key'      => env('AWS_ACCESS_KEY_ID'),
+            'secret'   => env('AWS_SECRET_ACCESS_KEY'),
+            'region'   => env('AWS_DEFAULT_REGION'),
+            'bucket'   => env('AWS_BUCKET'),
+            'url'      => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
         ],
 
