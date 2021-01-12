9a3a4eff0906aa048bb0095038f7d7c9303dabe9
Hugo Jeller <hugo_jeller@hotmail.com>
2021-01-12 06:24:13 -0400

    Fixed spacing in AdminProducerIndex

--- resources/js/pages/admin/AdminProducerIndex.tsx
+++ resources/js/pages/admin/AdminProducerIndex.tsx
@@ -1,17 +1,20 @@
 import React            from "react";
 import {WorkInProgress} from "../../components/ui/WorkInProgress";
+import {PagePadding}    from "../../containers/PagePadding";
 
 export const AdminProducerIndex: React.FC = () => {
-    return <div className="py-4 space-y-16 flex flex-col min-h-full">
-        {/* Header */}
-        <div className="text-center space-y-4">
-            <h1 className="text-2xl text-gray-700 font-bold">Em desenvolvimento!</h1>
-            <h2 className="text-gray-400">O conteúdo dessa página ainda não está pronto, tente novamente mais tarde.</h2>
-        </div>
+    return <PagePadding className="h-full flex flex-col items-center justify-center">
+        <div className="space-y-8">
+            {/* Header */}
+            <div className="text-center space-y-4">
+                <h1 className="text-2xl text-gray-700 font-bold">Em desenvolvimento!</h1>
+                <h2 className="text-gray-400">O conteúdo dessa página ainda não está pronto, tente novamente mais tarde.</h2>
+            </div>
 
-        {/* Illustration */}
-        <div className="mx-auto flex flex-shrink-0 items-center justify-center h-64 w-64">
-            <WorkInProgress/>
+            {/* Illustration */}
+            <div className="mx-auto flex flex-shrink-0 items-center justify-center h-64 w-64">
+                <WorkInProgress/>
+            </div>
         </div>
-    </div>
+    </PagePadding>
 };
