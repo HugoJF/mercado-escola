43ba532cf1184c75686743783084505d77c5bdd2
Hugo Jeller <hugo_jeller@hotmail.com>
2021-01-12 03:05:20 -0400

    Added offline screen

--- resources/js/routes/RootRoutes.tsx
+++ resources/js/routes/RootRoutes.tsx
@@ -14,25 +14,28 @@ import {AccountRoutes}         from "./AccountRoutes";
 import {CartRoutes}            from "./CartRoutes";
 import {AdminRoutes}           from "./AdminRoutes";
 import {OnBoardingRoutes}      from "./OnBoardingRoutes";
+import {Online}                from "../components/ui/Online";
 
 export const RootRoutes: React.FC = () => {
     return <Splash>
-        <Overlay>
-            <SwitchWithTransitions>
-                <Route path="/login" children={<Login/>}/>
-                <Route path="/register" children={<Register/>}/>
+        <Online>
+            <Overlay>
+                <SwitchWithTransitions>
+                    <Route path="/login" children={<Login/>}/>
+                    <Route path="/register" children={<Register/>}/>
 
-                <ProtectedRoute path="/home" children={<HomeRoutes/>}/>
-                <ProtectedRoute path="/on-boarding" children={<OnBoardingRoutes/>}/>
-                <ProtectedRoute path="/pedidos" children={<OrdersRoutes/>}/>
-                <ProtectedRoute path="/produtos" children={<ProductsRoutes/>}/>
-                <ProtectedRoute path="/favoritos" children={<FavoritesRoutes/>}/>
-                <ProtectedRoute path="/conta" children={<AccountRoutes/>}/>
-                <ProtectedRoute path="/carrinho" children={<CartRoutes/>}/>
-                <ProtectedRoute path="/admin" children={<AdminRoutes/>}/>
+                    <ProtectedRoute path="/home" children={<HomeRoutes/>}/>
+                    <ProtectedRoute path="/on-boarding" children={<OnBoardingRoutes/>}/>
+                    <ProtectedRoute path="/pedidos" children={<OrdersRoutes/>}/>
+                    <ProtectedRoute path="/produtos" children={<ProductsRoutes/>}/>
+                    <ProtectedRoute path="/favoritos" children={<FavoritesRoutes/>}/>
+                    <ProtectedRoute path="/conta" children={<AccountRoutes/>}/>
+                    <ProtectedRoute path="/carrinho" children={<CartRoutes/>}/>
+                    <ProtectedRoute path="/admin" children={<AdminRoutes/>}/>
 
-                <Redirect to="/home"/>
-            </SwitchWithTransitions>
-        </Overlay>
+                    <Redirect to="/home"/>
+                </SwitchWithTransitions>
+            </Overlay>
+        </Online>
     </Splash>
 };
