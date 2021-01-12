a1ac67a311bd4af2fc9f01fe8911dc9d78a534d6
Hugo Jeller <hugo_jeller@hotmail.com>
2021-01-12 03:19:45 -0400

    Added padding in offline screen

--- resources/js/components/ui/Online.tsx
+++ resources/js/components/ui/Online.tsx
@@ -28,17 +28,22 @@ export const Online: React.FC = ({children}) => {
         <CSSTransition
             key={String(online)}
             classNames="fade"
-            timeout={10000}
+            timeout={500}
         >
             <Container>
                 {online ?
                     <>{children}</>
                     :
-                    <div className="space-y-8 w-full min-h-full flex flex-col items-center justify-center bg-gradient-to-br from-primary-500 to-primary-700 justify-center">
+                    <div className="px-8 space-y-8 w-full min-h-full flex flex-col items-center justify-center bg-gradient-to-br from-primary-500 to-primary-700 justify-center">
+                        {/* Icon */}
                         <WifiOff size={48} className="text-white"/>
+
+                        {/* Header */}
                         <h1 className=" text-center text-4xl text-white font-bold leading-none tracking-wide">
                             Você está offline!
                         </h1>
+
+                        {/* Description */}
                         <p className="text-center text-primary-100 tracking-tighter">
                             Precisamos de conexão com a internet para funcionar. Por favor verifique sua conexão para continuar!
                         </p>
