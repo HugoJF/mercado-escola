94dec6e873c46a5ddbb927227d40f9fa8fc9dfa9
Hugo Jeller <hugo_jeller@hotmail.com>
2021-02-01 17:53:13 -0400

    Fixes ImageHolder flashes when image is loaded

--- resources/js/components/ui/ImageHolder.tsx
+++ resources/js/components/ui/ImageHolder.tsx
@@ -1,9 +1,25 @@
-import React           from "react";
-import {LazyLoadImage} from "./Image";
+import React, {useEffect, useState} from "react";
+import {LazyLoadImage}              from "./Image";
+import {Breathing}       from "react-shimmer";
+import classNames        from "classnames";
+import useTimeout        from "use-timeout";
 
 export type ImageHolderProps = React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>
 
 export const ImageHolder: React.FC<ImageHolderProps> = ({src}) => {
+    const [loaded, setLoaded] = useState(false);
+    const [visible, setVisible] = useState(false);
+
+    /*
+        This timeout is used to give React enough time to mount the image
+        with 0 opacity, so we can change it to 100% and give it a transition.
+        Any duration above 1 should be enough to cause the update to be executed
+        in another render (thus allowing the transition to happen).
+     */
+    useTimeout(() => {
+        setVisible(true);
+    }, loaded ? 10 : null);
+
     return <div className="relative pb-3/5">
         <div className="absolute w-full h-full bg-gradient-to-b from-gray-200 to-gray-300 rounded-lg animate-pulse"/>
 
@@ -12,13 +28,14 @@ export const ImageHolder: React.FC<ImageHolderProps> = ({src}) => {
         </div>}
 
         {src && <LazyLoadImage
-            fadeIn
+            onLoad={() => setLoaded(true)}
             src={src}
-            fallback={
-                <div className="absolute w-full h-full bg-gradient-to-b from-gray-200 to-gray-300 rounded-lg animate-pulse"/>
-            }
+            fallback={<></>}
             NativeImgProps={{
-                className:"absolute w-full h-full mb-4 rounded-lg shadow-md object-cover"
+                className: classNames("transition-all duration-150 absolute w-full h-full mb-4 rounded-lg shadow-md object-cover", {
+                    'opacity-0': !visible,
+                    'opacity-100': visible,
+                })
             }}
         />}
     </div>
