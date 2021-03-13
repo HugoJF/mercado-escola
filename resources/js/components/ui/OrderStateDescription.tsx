5710516c44957672a30efb8e5ac80a4b421cbd5a
Hugo Jeller <hugo_jeller@hotmail.com>
2021-03-12 21:15:40 -0400

    Improve order state description

--- resources/js/components/ui/OrderStateDescription.tsx
+++ resources/js/components/ui/OrderStateDescription.tsx
@@ -6,10 +6,10 @@ export type OrderStateDescriptionProps = {
 }
 
 export const OrderStateDescriptionMapping: { [state in OrderStateEnum]: string } = {
-    [OrderStateEnum.ACCEPTED]: 'O pedido foi aceito pela administração e será preparado no final da abertura.',
+    [OrderStateEnum.ACCEPTED]: 'O pedido foi aceito pelo mercado e será preparado no final da abertura.',
     [OrderStateEnum.CANCELLED]: 'O pedido foi cancelado.',
     [OrderStateEnum.DELIVERED]: 'O pedido foi entregue no endereço especificado.' ,
-    [OrderStateEnum.PENDING]: 'O pedido foi recebido pelo sistema mas ainda não foi confirmado pela administração.',
+    [OrderStateEnum.PENDING]: 'O pedido foi recebido pelo sistema mas ainda não foi confirmado pelo mercado.',
     [OrderStateEnum.READY]: 'O pedido está pronto para ser retirado.',
     [OrderStateEnum.REJECTED]: 'O pedido foi rejeitado.',
     [OrderStateEnum.SHIPPING]: 'O pedido está sendo entregue no endereço especificado.',
