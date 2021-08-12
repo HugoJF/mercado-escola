96c2edd056d0432e3ce9c5c495266778abfe4324
Hugo Jeller Ferreira <hugo_jeller@hotmail.com>
2021-08-11 23:42:17 -0400

    Add mysql-client dependency

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -20,7 +20,7 @@ then
     mc alias set market $MINIO_ENDPOINT $MINIO_ACCESS_KEY_ID $MINIO_SECRET_ACCESS_KEY
 
     echo "Syncing production Minio to staging"
-    mc mirror market/MINIO_SYNC_BUCKET market/$MINIO_BUCKET
+    mc mirror market/$MINIO_SYNC_BUCKET market/$MINIO_BUCKET
 
     echo "Dumping production database to staging"
     mysqldump --host $DB_ORIGIN_HOST -u $DB_ORIGIN_USERNAME --password=$DB_ORIGIN_PASSWORD --port $DB_ORIGIN_PORT $DB_ORIGIN_DATABASE | mysql --host $DB_HOST -u $DB_USERNAME --password=$DB_PASSWORD --port $DB_PORT $DB_DATABASE
