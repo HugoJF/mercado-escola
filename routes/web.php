3c08be71cd956c1d1ba75ef1d1f8230b9232d448
Hugo Jeller Ferreira <hugo_jeller@hotmail.com>
2022-02-18 14:27:28 -0400

    Setup Serverless

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@ Route::get('email', function () {
     return new \App\Mail\OrderCreated(\App\Models\Order::latest()->first());
 });
 
+Route::get('hello', fn () => ['data' => 'Hello world']);
+
 Route::get('{all?}', [App\Http\Controllers\HomeController::class, 'index'])->where('all', '([A-z\d\-\/_.]+)?');
 
